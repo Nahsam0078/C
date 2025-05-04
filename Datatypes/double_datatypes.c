cb51629e6450b8d8e3b2c3b7a1a71bcc50bc89da
Nahsam Ahammed <nahsamahammed@gmail.com>
2025-05-04 22:22:37 +0400

    practised a new datatype double/long float

---
+++ Datatypes/double_datatypes.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+     double pi = 3.145696;
+     double something = 56988752235555.2365;
+     printf("your pi full value is %lf \n" , pi);
+     printf("your somethiing is %.2lf\n",something);// %.2lf means 2 decimal points
+     printf("your somethiing is %.3lf\n",something);// %.3lf means 3 decimal points
+
+
+}
