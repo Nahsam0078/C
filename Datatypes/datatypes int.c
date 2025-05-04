63e9081ad4b5634c244bbbd2c467433434d8aab2
Nahsam Ahammed <nahsamahammed@gmail.com>
2025-05-04 22:05:24 +0400

    Added Datatypes

---
+++ Datatypes/datatypes int.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    int age = 23;
+    int year = 2025;
+    printf("the year is %d\n", year);
+    printf("my age is %d years old", age);
+    return 0;
+}
---
+++ Datatypes/datatypes_float.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    float pi = 3.14;
+    float gpa = 3.3;
+    printf("your pi is %f\n",pi);
+    printf("your gpa is %f\n", gpa);
+    return 0;
+}
