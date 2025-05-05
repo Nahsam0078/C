8d5ab1740bae8e0707c981dae4e1998f50bc1cce
Nahsam Ahammed <nahsamahammed@gmail.com>
2025-05-05 19:55:50 +0400

    created 2 files and learned array and bool

---
+++ Datatypes/datatypes_array.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    char name[] ="Sam Crack";
+    char food[] = "pie";
+    printf("my nam eis %s\n", name);
+    printf("may favourite food is %s\n", food);
+    return 0;
+}
---
+++ Datatypes/datatypes_bool.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int main(){
+    bool isOnline = 1;
+    if(isOnline){
+        printf("you are online\n");}
+    else{
+        printf("you are offline\n");
+    }
+    return 0;
+
+}
