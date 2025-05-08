a21bcfc7103bfcd91d1112ae8023358aadd5461b
Nahsam Ahammed <nahsamahammed@gmail.com>
2025-05-08 18:00:13 +0400

    2nd update

--- Shopping Cart/shopping cart.c
+++ Shopping Cart/shopping cart.c
@@ -5,6 +5,15 @@ int main(){
     int quantity = 0;
     char currency = '$';
     float total = 0.0f;
-    
+
+    printf("what would you like to buy?\n");
+    fgets(item, sizeof(item), stdin);
+    printf("what is the price for each item?\n");
+    scanf("%f",&price);
+    printf("how many would you like to buy?\n");
+    scanf("%d", &quantity);
+
+    return 0;
+
 
 }
